86b11f8b6c32550d0d421e4c896149606f2c9dd3
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2022-01-06 12:10:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpreadSheetConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //.Net support in progress 
+        }
+    }
+}
